e6baf714b365d7879b9443a49b0ea2cbae409fa4
Buck Tower <bucktower@utexas.edu>
2026-01-17 08:00:56 +0000

    Add dist

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
+  base: "/the-exit-interview/",
   plugins: [
     react(),
     runtimeErrorOverlay(),
@@ -22,7 +23,7 @@ export default defineConfig({
   },
   root: path.resolve(__dirname, "client"),
   build: {
-    outDir: path.resolve(__dirname, "dist/public"),
+    outDir: path.resolve(__dirname, "dist"),
     emptyOutDir: true,
   },
   // Add support for large models and audio files
